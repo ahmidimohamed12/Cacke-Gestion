304fd121c178ea193eb2e679012b6d3f52f3e919
ahmidimohamed12 <ahmidinador123@gmail.com>
2020-12-11 17:00:23 +0100

    .

--- cacke.Android/obj/Debug/android/environment.x86.s
+++ cacke.Android/obj/Debug/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"f7521457-22f9-4bfc-9ac5-bad6ffeb6cd8"
+	.asciz	"20ccfb7b-a954-49c0-8f48-1cb643e3efde"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
