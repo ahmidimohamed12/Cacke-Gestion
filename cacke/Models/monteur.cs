7040c5640488d55ebd6382dfa753288838f57fdb
ahmidimohamed12 <ahmidinador123@gmail.com>
2020-12-16 09:55:15 +0100

    add

--- cacke/Models/monteur.cs
+++ cacke/Models/monteur.cs
@@ -3,8 +3,41 @@ namespace cacke.Models
 {
     public class monteur
     {
-        public monteur()
-        {
-        }
+
+        public int id { get; set; }
+
+        public string nom { get; set; }
+
+        public string datec { get; set; }
+
+        public string adress { get; set; }
+
+        public string tlf { get; set; }
+
+        public Nullable<int> profit { get; set; }
+
+        public string sociter { get; set; }
+
+        public string kvk { get; set; }
+
+        public string btw { get; set; }
+
+        public string specialite { get; set; }
+
+        public string latitude { get; set; }
+
+        public string longtitud { get; set; }
+
+        public string pass { get; set; }
+
+        public string usrname { get; set; }
+
+        public string token { get; set; }
+
+        public Nullable<int> isb { get; set; }
+
+        public Nullable<int> boos { get; set; }
+
+
     }
 }
--- cacke/Models/works.cs
+++ cacke/Models/works.cs
@@ -6,5 +6,59 @@ namespace cacke.Models
         public works()
         {
         }
+
+
+        public int id { get; set; }
+
+        public Nullable<DateTime> stardate { get; set; }
+
+        public string company { get; set; }
+
+        public string mediator { get; set; }
+
+        public string callcenter { get; set; }
+
+        public string customer { get; set; }
+
+        public Nullable<double> sales { get; set; }
+
+        public Nullable<int> profit { get; set; }
+
+        public string material { get; set; }
+
+        public string adress { get; set; }
+
+        public string statuss { get; set; }
+
+        public string paidby { get; set; }
+
+        public string calculated { get; set; }
+
+        public string phone { get; set; }
+
+        public string moreinfo { get; set; }
+
+        public Nullable<double> sales2 { get; set; }
+
+        public string paydby2 { get; set; }
+
+        public  string lastupdate { get; set; }
+
+        public string latitude { get; set; }
+
+        public string longtitud { get; set; }
+
+        public string accepted { get; set; }
+
+        public Nullable<int> monteur { get; set; }
+
+        public Nullable<int> website { get; set; }
+
+        public Nullable<int> gbits { get; set; }
+
+
+        public virtual monteur monteur1 { get; set; }
+
+        public  virtual website website1 { get; set; }
     }
 }
